8ed8515d0cdbd8f57f3d869ad02f21b037019f52
Neto Rodrigues <accounts@rodrigsneto.com>
2024-04-16 22:26:44 -0300

    refactor: redefinindo estruturas de rotas

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/plantoes', [DutyController::class, 'read'])->name('duty.index');
 Route::get('/novo', [DutyController::class, 'create'])->name('duty.create');
 Route::post('/plantao', [DutyController::class, 'store'])->name('duty.store');
+Route::get('/editar/{plantao}/', [DutyController::class, 'edit'])->name('duty.edit');
+Route::put('/editar/{plantao}/update', [DutyController::class, 'update'])->name('duty.update');
 
 // Atendimentos dos plantões
-Route::get('/atendimentos', [DutyController::class, 'dutyservices_read'])->name('dutyservices.index');
-Route::get('/atendimentos/novo', [DutyController::class, 'dutyservice_create'])->name('dutyservices.create');
-Route::post('/atendimentos', [DutyController::class, 'dutyservice_store'])->name('dutyservices.store');
+Route::get('/atendimentos/view/{plantao}/', [DutyController::class, 'dutyservices_read'])->name('dutyservices.index');
+Route::get('/atendimentos/novo/{plantao}/', [DutyController::class, 'dutyservice_create'])->name('dutyservices.create');
+Route::post('/atendimentos/{plantao}/', [DutyController::class, 'dutyservice_store'])->name('dutyservices.store');
+Route::delete('/atendimentos/{plantao}/delete/{atendimento}', [DutyController::class, 'dutyservice_delete'])->name('dutyservices.delete');
 
